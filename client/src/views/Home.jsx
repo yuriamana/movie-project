42d9c487e1eeef58c8375294cf36013e1fadd07a
Yuki Kato <katoyuki0113@gmail.com>
2021-11-12 17:56:52 +0100

    Yuki

--- client/src/views/Home.jsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-    return (
-      <div className="page home">
-        <h1 className="title">Welcome...Home</h1>
-        <img src="https://media.giphy.com/media/9LQHvkbIzTSLe/giphy.gif" alt="fun pic" />
-      </div>
-    );
-  }
-  

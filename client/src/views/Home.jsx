6ba891d23f276dcf037c750fccb76c04b421eab0
Joey6Hang <flamincos517@gmail.com>
2021-11-12 09:32:39 +0100

    les views

---
+++ client/src/views/Home.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+      <div className="page home">
+        <h1 className="title">Welcome...Home</h1>
+        <img src="https://media.giphy.com/media/9LQHvkbIzTSLe/giphy.gif" alt="fun pic" />
+      </div>
+    );
+  }
+  

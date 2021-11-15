79fc4236ab5e620d622845d1a951f4882dafd277
Yuki Kato <katoyuki0113@gmail.com>
2021-11-15 18:31:08 +0100

    added form recommendation

---
+++ client/src/components/form/FormRecommendation.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+// import APIHandler from "../../api/APIHandler";
+
+export default class FormRecommendation extends Component {
+  state = {
+    mood: [],
+  };
+
+  handleSubmit(event) {
+    alert("Your mood is : " + this.state.value);
+    event.preventDefault();
+  }
+
+  handleChange() {}
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label className="label" htmlFor="mood">
+          What is your mood ?
+          <select value={this.state.value} onChange={this.handleChange}>
+            <option value="Happy">Happy</option>
+            <option value="Suicidal">Suicidal</option>
+            <option value="Romantic">Romantic</option>
+            <option value="Want to laugh">Want to laugh</option>
+            <option value="Want to be scared">Want to be scared</option>
+          </select>
+        </label>
+        <input type="submit" value="Envoyer" />
+      </form>
+    );
+  }
+}
--- client/src/views/MovieDetail.jsx
+++ client/src/views/MovieDetail.jsx
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
 import APIHandler from "./../api/APIHandler";
+// import FormCreateComment from "./FormCreateComment";
+import { Container, Row, Col } from "react-bootstrap";
+// import './../styles/MovieDetail.css'
 import FormCreateComment from "../components/form/FormCreateComment";
 
 export default class MovieDetail extends Component {
@@ -10,14 +13,11 @@ export default class MovieDetail extends Component {
     director: "",
     duration: "",
     plot: "",
-    actors: [
-      {
+    actorList: [{
         image: "",
-        name: "",
-      },
-    ],
-    genre: [
-      {
+        name: ""
+    }],
+    genreList: [{
         value: "",
       },
     ],
@@ -26,51 +26,62 @@ export default class MovieDetail extends Component {
   };
 
   async componentDidMount() {
-    console.log(this);
-    console.log(this.props.location.movieId);
-    //this.props are properties you give to an object when you create so you can access/use them
-    //for ex Link with a props "to=" create a new object (movie detail) and provide its properties
-    APIHandler.get(`/movies/${this.props.location.movieId}`)
-      .then(({ data }) => {
-        console.log("movies", data);
+    console.log(this)
+      console.log(this.props.location.movieId)
+//this.props are properties you give to an object when you create so you can access/use them
+//for ex Link with a props "to=" create a new object (movie detail) and provide its properties
+  APIHandler.get(`/movie/${this.props.match.params.id}`)
+    .then(({ data }) => {
         this.setState({
           title: data.title,
-          year: data.year,
+          year: data.year.split(0,1),
           director: data.directors,
           duration: data.runtimeMins,
           plot: data.plot,
           image: data.image,
-          // actors: data.actorList[1].name,
-          // genre: data.genreList[0].value,
+          actorList: data.actorList,
+          genreList: data.genreList,
           imDbRating: data.imDbRating,
           usersRating: data.usersRating,
         });
       })
       .catch((apiErr) => console.error(apiErr));
-  }
-  render() {
-    return (
-      <div>
-        <h3>Title: {this.state.title}</h3>
-        <img src={this.state.image} alt="one-movie" width="200px" />
-        <span>Year: {this.state.year}</span>
-        <br />
-        <span>Director: {this.state.director}</span>
-        <br />
-        <span>Duration: {this.state.duration}</span>
-        <br />
-        <span>Plot: {this.state.plot}</span>
-        <br />
-        {/* <span>Actors: {this.state.actors}</span>
-        <br />
-        <span>Genre: {this.state.genre}</span>
-        <br /> */}
-        <span>IMDB rating: {this.state.imDbRating}</span>
-        <br />
-        <span>User's rating: {this.state.usersRating}</span>
-        <br />
-        <FormCreateComment movieId={this.props.location.movieId} />
-      </div>
+    }
+    render() {
+      console.log(this.props)
+      console.log(this.state.actorList);
+      return (
+        <Container>
+        <br/>
+        <h1>Title: {this.state.title} </h1> <span>({this.state.year})</span>
+        <br/>
+        <Row className="bodydetail">
+          <Col className="colImag">
+            <img src={this.state.image} alt="one-movie" width="200px"/>
+          </Col>  
+
+          <Col>
+            <span>Director: {this.state.director}</span>
+            <br />
+            <span>Duration: {this.state.duration}min</span>
+            <br />
+            <br />
+            <span>Actors: {this.state.actorList.map(actor => <p>{actor.name}</p>)}</span>
+            <br />
+            <span>Genre: {this.state.genreList.key}</span>
+            <span>IMDB rating: {this.state.imDbRating}</span>
+            <br />
+            <span>User's rating: {this.state.usersRating}</span>
+          </Col>  
+            <div className="plot">
+            <section>Plot: {this.state.title}</section>
+            <h5>{this.state.plot}</h5>
+            </div>
+            <Row>
+        <FormCreateComment movieId={this.props.location.movieId}/>
+        </Row>
+        </Row>
+      </Container>
     );
   }
 }

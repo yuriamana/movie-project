466370ebbe46fb215d72601d56df405ce3302652
Yuki Kato <katoyuki0113@gmail.com>
2021-11-15 09:43:40 +0100

    joey

--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Nav from "./Nav";
+import Nav from "../components/Nav";
 import { Link } from "react-router-dom";
 import APIHandler from "./../api/APIHandler";
 //styles

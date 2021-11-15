e8d19381cd276d78bbaacf5a82efc2c68b6e1d33
Yuki Kato <katoyuki0113@gmail.com>
2021-11-15 09:51:40 +0100

    Joey

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, { Component } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { Link } from "react-router-dom";
 import './../styles/nav.css'
 import {Container,Navbar, FormControl, Form, Button} from 'react-bootstrap'
+import Search from "./Search"
+// import AllMovies from './AllMovies';
 
-export default function Nav (){
+export default class Nav extends Component {
+    state = {
+        searchField:'',
+        movies:[],
+        stats:[]
+    }
+    render(){
+        const {stats,searchField} =this.state
+        const filterMovies = stats.filter(movie => (
+            movie.title.toLowerCase().includes(searchField.toLowerCase())
+        ))
     return (
         <Navbar bg="dark" expand="sm">
         <Container>
@@ -12,16 +24,19 @@ export default function Nav (){
         <Link to='/'><img src="/imag/movie logo.png" alt="movielogo" className="movielogo"/></Link>
         </Navbar.Brand>
         <Form className="search">
-            <FormControl
+            {/* <FormControl
             type="search"
             placeholder='search'
             id='gsearch'
-            aria-label='Search'/>
-            <Button variant="outlien-success" className="Sbutton">Search</Button>
+            aria-label='Search'/> */}
+            <Search placeholder="Search.." handleChange={(e) => this.setState({searchField:e.target.value})}/>
+            <Button type="submit" variant="outlien-success" className="Sbutton">Search</Button>
+            <AllMovies stats = {filterMovies} />
         </Form>
-        <Link to="/login" className="login">Login</Link>
+        <Link to="/signin" className="login">Signin</Link>
         <Link to="/signup" className="login">Signup</Link>
     </Container>
     </Navbar>
     )
+    }
 }

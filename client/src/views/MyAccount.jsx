738d23d3b9990d43128b12ef73353b9db2fbdaea
Priscilla Gasztowtt <pgasztowtt@SIA-Conseil.local>
2021-11-16 20:08:40 +0100

    nav search myaccount app.js

--- client/src/views/MyAccount.jsx
+++ client/src/views/MyAccount.jsx
@@ -1,30 +1,35 @@
-import React, {Component} from "react"; 
-import APIHandler from "./../api/APIHandler"; 
+import React, { Component } from "react";
+// import APIHandler from "./../api/APIHandler";
+import { withAuth } from "./../auth/UserContext.js";
 
-export default class MyAccount extends Component {
-    state = {
-      commentedMovies: [],
-    };
-  
-    async componentDidMount() {
-      APIHandler.get("/movies/:id")
-      
-        .then(({ data }) => {
-          console.log("commentedMovies", data);
-          this.setState({
-            commentedMovies: data,
-          });
-        })
-        .catch((apiErr) => console.error(apiErr));
-    }
-    render() {
-        return (
-            <div>Commented movies
-            {/* this.state.commentedMovies.map(movie => {
+class MyAccount extends Component {
+  state = {
+    commentedMovies: [],
+  };
+
+  async componentDidMount() {
+    console.log(this.props)
+    // return;
+    // APIHandler.get("/movies/:id")
+
+      // .then(({ data }) => {
+      //   console.log("commentedMovies", data);
+      //   this.setState({
+      //     commentedMovies: data,
+      //   });
+      // })
+      // .catch((apiErr) => console.error(apiErr));
+  }
+  render() {
+    return (
+      <div>
+        Commented movies
+        {/* this.state.commentedMovies.map(movie => {
             }) */}
-            </div>
-        )
-    }
+      </div>
+    );
+  }
 }
 
-// //on veut display seulement les films que le user a commenté/raté
+export default withAuth(MyAccount); // HOC => encapsule ta classe et lui donne en props les infos de l'auth
+//on veut display seulement les films que le user a commenté/raté

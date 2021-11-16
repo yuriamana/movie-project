d5260804b866f7364eb10bc0bf2963e1c524e3f9
Priscilla Gasztowtt <pgasztowtt@SIA-Conseil.local>
2021-11-16 09:25:43 +0100

    route search

---
+++ server/routes/search.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const router = new express.Router();
+const MovieModel = require("../model/Movie");
+
+router.get("/search", async (req, res, next) => {
+  const regExp = new RegExp(req.query.q, "i");
+  const movieSearch = MovieModel.find({
+    name: regExp
+  });
+    .populate({
+      path: "movies",
+      match: { name: regExp }
+    });
+
+  Promise.all([movieSearch])
+    .then(dbRes => res.json({movies: dbRes[0]}))
+    .catch(next);
+});
+
+module.exports = router;
